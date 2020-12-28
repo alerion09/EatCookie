0c8c5bec9fdeffaa5578dda03523471adec064e6
Kajetan Kuzma <kajetankuzma@gmail.com>
2020-12-28 19:10:48 +0100

    Created createHighScoresTable function responisble for creating HTML table contain high scores data.

--- www/js/highscores_script.js
+++ www/js/highscores_script.js
@@ -3,8 +3,19 @@ function appStart()
 {
     const newGameButton = document.querySelector('.button_new_game');
     newGameButton.addEventListener('click', show_index, false);
+    createHighScoresTable();
 }
 function show_index()
 {
     document.location.href = 'index.html';
+}
+function createHighScoresTable()
+{
+    const highScoresTbody = document.querySelector('.highScores_tbody');
+    const objHighScores = JSON.parse(window.localStorage.getItem('highscores'));
+    let i;
+    for (i = 0; objHighScores.length != i; i++)
+    {
+        highScoresTbody.innerHTML+= `<tr><td>${objHighScores[i].nick}</td><td>${objHighScores[i].score}</td></tr>`;
+    }
 }
